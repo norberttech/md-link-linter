3579654247f3c2e479df5f0723f158cab34a04a2
Norbert Orzechowicz <norbert@orzechowicz.pl>
2019-10-26 22:35:53 +0200

    CS Fixes

--- tests/MDLinkLinter/Tests/Directory/MDFileIteratorTest.php
+++ tests/MDLinkLinter/Tests/Directory/MDFileIteratorTest.php
@@ -29,9 +29,9 @@ final class MDFileIteratorTest extends TestCase
             \iterator_to_array($iterator->iterate())
         );
 
-        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles));
-        $this->assertTrue(\in_array('LICENSE.md', $mdFiles));
-        $this->assertTrue(\in_array('file_with_invalid_link.md', $mdFiles));
+        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles, true));
+        $this->assertTrue(\in_array('LICENSE.md', $mdFiles, true));
+        $this->assertTrue(\in_array('file_with_invalid_link.md', $mdFiles, true));
         $this->assertCount(3, $mdFiles);
     }
 
@@ -46,8 +46,8 @@ final class MDFileIteratorTest extends TestCase
             \iterator_to_array($iterator->iterate())
         );
 
-        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles));
-        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles));
+        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles, true));
+        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles, true));
         $this->assertCount(2, $mdFiles);
     }
 
@@ -62,7 +62,7 @@ final class MDFileIteratorTest extends TestCase
             \iterator_to_array($iterator->iterate())
         );
 
-        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles));
+        $this->assertTrue(\in_array('file_with_valid_link.md', $mdFiles, true));
         $this->assertCount(1, $mdFiles);
     }
 }
