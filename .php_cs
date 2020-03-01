49beb2eef24abc973bbc521a6eb171cc62dab7d0
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-03-01 19:07:29 +0100

    Chaned php files header

--- .php_cs
+++ .php_cs
@@ -3,7 +3,7 @@
 $fileHeaderComment = <<<COMMENT
 This file is part of the Markdown Link Linter library.
 
-(c) Norbert Orzechowicz <norbert@orzechowicz.pl>
+(c) Norbert Orzechowicz <contact@norbert.tech>
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
--- tests/MDLinkLinter/Tests/Directory/MDFileIteratorTest.php
+++ tests/MDLinkLinter/Tests/Directory/MDFileIteratorTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the Markdown Link Linter library.
  *
- * (c) Norbert Orzechowicz <norbert@orzechowicz.pl>
+ * (c) Norbert Orzechowicz <contact@norbert.tech>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
